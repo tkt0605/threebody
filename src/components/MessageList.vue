189f3c1cd15e77b6daa552c60793303b249407ae
tkt0605 <takato.komada2005@outlook.jp>
2026-07-11 07:50:22 +0900

    style: メッセージ一覧のpy-6を削除

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -28,7 +28,7 @@ function isOrphaned(msg: Message, index: number): boolean {
 
 <template>
   <!-- <div ref="container" class="flex-1 overflow-y-auto px-4 py-6 space-y-3 scroll-smooth"> -->
-  <div ref="container" class="flex-1 overflow-y-auto py-6 scroll-smooth [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-gray-600 [&::-webkit-scrollbar-thumb]:rounded-full">
+  <div ref="container" class="flex-1 overflow-y-auto scroll-smooth [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-gray-600 [&::-webkit-scrollbar-thumb]:rounded-full">
       <div v-if="messages.length === 0 && !draftMessage" class="flex h-full items-center justify-center">
         <div class="text-black/20 dark:text-white/40 text-sm select-none">
           最初の会話を始めましょう。
